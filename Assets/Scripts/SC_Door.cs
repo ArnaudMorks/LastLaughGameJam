d3a4a1039df90cb70af253a0b6305b834ea7eaa0
ArnaudMorks <arnaud.morks@gmail.com>
2024-01-27 00:41:27 +0100

    A Deur met Audio

--- Assets/Scripts/SC_Door.cs
+++ Assets/Scripts/SC_Door.cs
@@ -15,13 +15,26 @@ public class SC_Door : MonoBehaviour
     [SerializeField] private bool isOpen = false;       //hiermee kan je ook bepalen of die open of dicht moet beginnen
     private bool playerInRange = false;
 
+    [SerializeField] private AudioClip audioOpen;
+    [SerializeField] private AudioClip audioClose;
+    [SerializeField] private AudioSource audioSource;
 
     void Start()
     {
         playerCollider = FindObjectOfType<SC_CharacterController2D>().GetComponent<CapsuleCollider2D>();
 
-        if (isOpen == true) { DoorOpen(); }
-        else { DoorOpen(); }
+        if (isOpen == true)
+        {
+            spriteRenderer.sprite = openDoorsSprite;
+            doorCollider.enabled = false;
+            isOpen = true;
+        }
+        else
+        {
+            spriteRenderer.sprite = closedDoorSprite;           //zelfde code als bij Open en Close functies maar dan zonder geluid
+            doorCollider.enabled = true;
+            isOpen = false;
+        }
     }
 
 
@@ -56,6 +69,10 @@ public class SC_Door : MonoBehaviour
     {
         spriteRenderer.sprite = openDoorsSprite;
         doorCollider.enabled = false;
+        audioSource.clip = audioOpen;
+        audioSource.volume = Random.Range(0.8f, 1);
+        audioSource.pitch = Random.Range(0.8f, 1.2f);
+        audioSource.PlayOneShot(audioSource.clip);
         isOpen = true;
     }
 
@@ -63,6 +80,10 @@ public class SC_Door : MonoBehaviour
     {
         spriteRenderer.sprite = closedDoorSprite;
         doorCollider.enabled = true;
+        audioSource.clip = audioClose;
+        audioSource.volume = Random.Range(0.8f, 1);
+        audioSource.pitch = Random.Range(0.8f, 1.2f);
+        audioSource.PlayOneShot(audioSource.clip);
         isOpen = false;
     }
 
